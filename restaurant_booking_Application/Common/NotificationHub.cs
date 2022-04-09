ba54f5b0ad9784a537a2fc081bb3cedeb75d305c
allos <sadeosun@axxess.com>
2022-04-09 18:20:07 +0100

    Creating an restaurant api

---
+++ restaurant_booking_Application/Common/NotificationHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace restaurant_booking_Application.Common
+{
+    public class NotificationHub : Hub
+    {
+        private readonly IConfiguration _config;
+        public NotificationHub(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        HubConnection _connection = null;
+
+        private async Task ConnectToServer()
+        {
+            _connection = new HubConnectionBuilder()
+                .WithUrl(_config["HostUrl:Url"]).Build();
+            await _connection.StartAsync();
+        }
+    }
+}
